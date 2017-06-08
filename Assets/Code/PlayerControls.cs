8c3b6a31170ac9c339bbb53d563dc29690c7fb92
Ulises Taveras <Ulises Taveras>
2017-06-08 12:47:36 -0400

    Add project files.

---
+++ Assets/Code/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    public float speed = 5f;
+    
+	void Update ()
+    {
+		if (Input.GetKey(moveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+	}
+}
